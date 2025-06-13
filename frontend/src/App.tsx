1b8aeb6201f9f3141aa58aff76bee1d4c6b99371
LeonKindaTired <andrewsminki@gmail.com>
2025-06-13 21:32:13 +0300

    Start of project.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-blue-500">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nostrum
+      voluptatibus cum ducimus rerum dolores! Facilis quod, dolor deserunt
+      accusantium esse reiciendis beatae sequi recusandae voluptatum sit dolorum
+      aperiam ipsa!
+    </h1>
+  );
+}
+
+export default App;
